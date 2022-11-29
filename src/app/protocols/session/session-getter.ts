7779e04965355127f9f34ef9cfacc7bd3d0a1fdb
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2022-11-29 15:02:09 -0300

    refactor: removed session getter and setter from app layer

--- src/app/protocols/session/session-getter.ts
+++
@@ -1,5 +0,0 @@
-import { User } from 'domain/models/user'
-
-export interface SessionGetter {
-  get(): Promise<User>
-}
--- src/app/protocols/session/session-setter.ts
+++
@@ -1,9 +0,0 @@
-import { User } from 'domain/models/user'
-
-export type SessionSetterParams = {
-  user: User
-}
-
-export interface SessionSetter {
-  set(params: SessionSetterParams): Promise<void>
-}
