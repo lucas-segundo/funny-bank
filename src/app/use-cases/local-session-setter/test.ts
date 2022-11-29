3b169f067ada1274fa6b4bbaec36af27bbaa62b8
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2022-11-29 16:01:43 -0300

    test: throw unexpected error when something wrong happens

--- src/app/use-cases/local-session-setter/test.ts
+++ src/app/use-cases/local-session-setter/test.ts
@@ -1,5 +1,6 @@
 import { DatabaseSetterClientParams } from 'app/protocols/database/database-setter-client'
 import { mockDatabaseSetterClient } from 'app/protocols/database/database-setter-client/mock'
+import { UnexpectedError } from 'domain/errors/unexpected-error'
 import { User } from 'domain/models/user'
 import { mockSessionSetterParams } from 'domain/use-cases/session-setter/mock'
 import { LocalSessionSetter } from '.'
@@ -17,5 +18,17 @@ describe('LocalSessionSetter', () => {
       data: sessionSetterParams.user,
     }
     expect(databaseSetterClient.set).toBeCalledWith(setParams)
+  })
+
+  it('should throw unexpected error when something wrong happens', async () => {
+    const databaseSetterClient = mockDatabaseSetterClient()
+    databaseSetterClient.set.mockRejectedValueOnce(Error)
+
+    const sut = new LocalSessionSetter(databaseSetterClient)
+
+    const sessionSetterParams = mockSessionSetterParams()
+    const promise = sut.set(sessionSetterParams)
+
+    await expect(promise).rejects.toThrowError(UnexpectedError)
   })
 })
