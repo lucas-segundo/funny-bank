eef7a23a83fcbf3263323a2fa280dd3331763082
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2022-12-01 11:02:39 -0300

    feat: call getItem with right params in AsyncStorageClient

--- src/infra/databases/async-storage-client/index.ts
+++ src/infra/databases/async-storage-client/index.ts
@@ -1,10 +1,20 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import {
+  DatabaseGetterClient,
+  DatabaseGetterClientParams,
+} from 'app/protocols/database/database-getter-client'
+import {
   DatabaseSetterClient,
   DatabaseSetterClientParams,
 } from 'app/protocols/database/database-setter-client'
 
-export class AsyncStorageClient implements DatabaseSetterClient {
+export class AsyncStorageClient
+  implements DatabaseSetterClient, DatabaseGetterClient
+{
+  async get<Data>({ from }: DatabaseGetterClientParams): Promise<Data> {
+    await AsyncStorage.getItem(from)
+  }
+
   async set<Data>({
     into,
     data,
