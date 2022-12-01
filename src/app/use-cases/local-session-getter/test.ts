bb394a097beda74fab4234462a3d056d5fb19fee
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2022-12-01 10:46:55 -0300

    test: throw unexpected error when something wrong happens in LocalSessionGetter

--- src/app/use-cases/local-session-getter/test.ts
+++ src/app/use-cases/local-session-getter/test.ts
@@ -1,5 +1,6 @@
 import { DatabaseGetterClientParams } from 'app/protocols/database/database-getter-client'
 import { mockDatabaseGetterClient } from 'app/protocols/database/database-getter-client/mock'
+import { UnexpectedError } from 'domain/errors/unexpected-error'
 import { mockUser } from 'domain/models/user/mock'
 import { LocalSessionGetter } from '.'
 
@@ -27,5 +28,15 @@ describe('LocalSessionGetter', () => {
     const data = await sut.get()
 
     expect(data).toEqual(user)
+  })
+
+  it('should throw unexpected error when something wrong happens', async () => {
+    const databaseGetterClient = mockDatabaseGetterClient()
+    databaseGetterClient.get.mockRejectedValueOnce(Error)
+
+    const sut = new LocalSessionGetter(databaseGetterClient)
+    const promise = sut.get()
+
+    await expect(promise).rejects.toThrowError(UnexpectedError)
   })
 })
