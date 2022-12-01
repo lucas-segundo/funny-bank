1323e4748260944199de8e950528ea80ed0a84b0
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2022-12-01 11:38:44 -0300

    test: throw error if user session is not found in LocalSessionGetter

--- src/app/use-cases/local-session-getter/test.ts
+++ src/app/use-cases/local-session-getter/test.ts
@@ -1,22 +1,26 @@
 import { DatabaseGetterClientParams } from 'app/protocols/database/database-getter-client'
 import { mockDatabaseGetterClient } from 'app/protocols/database/database-getter-client/mock'
 import { UnexpectedError } from 'domain/errors/unexpected-error'
+import { UserSessionNotFoundError } from 'domain/errors/user-session-not-found-error'
 import { mockUser } from 'domain/models/user/mock'
 import { LocalSessionGetter } from '.'
 
 const makeSut = () => {
   const databaseGetterClient = mockDatabaseGetterClient()
   const sut = new LocalSessionGetter(databaseGetterClient)
+  const user = mockUser()
 
   return {
     databaseGetterClient,
     sut,
+    user,
   }
 }
 
 describe('LocalSessionGetter', () => {
   it('should call client with right params', async () => {
-    const { sut, databaseGetterClient } = makeSut()
+    const { sut, databaseGetterClient, user } = makeSut()
+    databaseGetterClient.get.mockResolvedValueOnce(user)
 
     await sut.get()
 
@@ -27,14 +31,21 @@ describe('LocalSessionGetter', () => {
   })
 
   it('should return user data', async () => {
-    const { sut, databaseGetterClient } = makeSut()
-
-    const user = mockUser()
+    const { sut, databaseGetterClient, user } = makeSut()
     databaseGetterClient.get.mockResolvedValueOnce(user)
 
     const data = await sut.get()
 
     expect(data).toEqual(user)
+  })
+
+  it('should throw error if user session is not found', async () => {
+    const { sut, databaseGetterClient } = makeSut()
+    databaseGetterClient.get.mockResolvedValueOnce(undefined)
+
+    const data = sut.get()
+
+    await expect(data).rejects.toThrow(UserSessionNotFoundError)
   })
 
   it('should throw unexpected error when something wrong happens', async () => {
