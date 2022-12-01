3113ed12abaa26767efbe10fa1bd0d5489b95f83
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2022-12-01 10:43:26 -0300

    test: return user data in LocalSessionGetter

--- src/app/use-cases/local-session-getter/test.ts
+++ src/app/use-cases/local-session-getter/test.ts
@@ -1,5 +1,6 @@
 import { DatabaseGetterClientParams } from 'app/protocols/database/database-getter-client'
 import { mockDatabaseGetterClient } from 'app/protocols/database/database-getter-client/mock'
+import { mockUser } from 'domain/models/user/mock'
 import { LocalSessionGetter } from '.'
 
 describe('LocalSessionGetter', () => {
@@ -13,5 +14,18 @@ describe('LocalSessionGetter', () => {
       from: 'userSession',
     }
     expect(databaseGetterlient.get).toBeCalledWith(getParams)
+  })
+
+  it('should return user data', async () => {
+    const databaseGetterlient = mockDatabaseGetterClient()
+
+    const user = mockUser()
+    databaseGetterlient.get.mockResolvedValueOnce(user)
+
+    const sut = new LocalSessionGetter(databaseGetterlient)
+
+    const data = await sut.get()
+
+    expect(data).toEqual(user)
   })
 })
